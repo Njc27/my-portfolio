91f8ce3fa1f3c9b44edf10d254d04de6752863e4
Njc27 <jc.n@northeastern.edu>
2024-07-23 00:36:56 -0400

    resume update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
-import Resume from './Assests/JajurChandrashekhar.Namitha.pdf'
+import Resume from './Assests/NamithaJC_Resume.pdf'
 import ME from './Assests/myPic.jpg'
 import linkedin from './Assests/linkedin.png'
 import github from './Assests/github.png'
@@ -36,7 +36,7 @@ const Home = () => {
                 <a href='https://www.linkedin.com/in/namitha-j-c-9b478416b/'><img src={linkedin} alt="linkedin" className='icons'/> <p>LinkedIn</p> </a>
                 <a href='https://github.com/Njc27'><img src={github} alt="github" className='icons'/> <p>Github</p></a>
                 <a href={Resume} download ><img src={resume} alt="resume" className='icons'/> <p>Resume</p></a>
-                <a href='./Contact'><img src={contact} alt="contact" className='icons'/> <p>Contact</p></a>  
+                <a href='#contact'><img src={contact} alt="contact" className='icons'/> <p>Contact</p></a>  
             </div>
         </div>
 
